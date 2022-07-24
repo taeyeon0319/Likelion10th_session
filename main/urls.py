1a2adde8775ee2f091ae9716490fed16cdfd254b
taeyeon0319 <xodus1349@likelion.org>
2022-07-24 17:05:36 +0900

    update

--- main/urls.py
+++ main/urls.py
@@ -4,8 +4,6 @@ from .views import *
 app_name = "main"
 urlpatterns = [
     path('', showmain, name="showmain"),
-    path('first/', first, name="first"),
-    path('second/', second, name="second"),
     path('<str:id>', detail, name="detail"),
     path('new/', new, name="new"),
     path('create/', create, name="create"),
